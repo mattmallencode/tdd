aaf34d9b25e2c20d347f03c99392d21a2195dc77
Matt Mallen <120355103@umail.ucc.ie>
2022-11-22 19:00:36 +0000

    Revert "TEST PASS: made testColourConstructor pass by creating a constructor with rgb params in Colour class."
    This reverts commit e88a6ddb63881af3de1741e1c3866ace19cb6d35.

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -1,13 +1,2 @@
 public class Colour {
-
-    int red;
-    int green;
-    int blue;
-
-    public Colour(int red, int green, int blue) {
-        this.red = red;
-        this.green = green;
-        this.blue = blue;
-    }
-
 }
