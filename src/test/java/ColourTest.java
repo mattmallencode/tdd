92d1df46f158d5e53d8edb526f245dd26d689285
Matt Mallen <120355103@umail.ucc.ie>
2022-11-22 18:47:02 +0000

    TEST FAIL: Created ColourTest class with a method for testing the Colour class' constructor, currently fails as there is no Colour class.

---
+++ src/test/java/ColourTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ColourTest {
+    @Test
+    void testColourConstructor() {
+        Colour colour = new Colour(255, 255, 255);
+    }
+}
