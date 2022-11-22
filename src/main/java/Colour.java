f1e140c4f218560ef88669b731474669b03fc237
Matt Mallen <120355103@umail.ucc.ie>
2022-11-22 19:04:47 +0000

    TEST PASS: Made testColourConstructor pass by created a constructor in Colour that takes three float params for red, green, and blue.

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -1,2 +1,12 @@
 public class Colour {
+
+    float red;
+    float green;
+    float blue;
+
+    public Colour(float red, float green, float blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
 }
