e88a6ddb63881af3de1741e1c3866ace19cb6d35
Matt Mallen <120355103@umail.ucc.ie>
2022-11-22 18:55:05 +0000

    TEST PASS: made testColourConstructor pass by creating a constructor with rgb params in Colour class.

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -1,2 +1,13 @@
 public class Colour {
+
+    int red;
+    int green;
+    int blue;
+
+    public Colour(int red, int green, int blue) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+    }
+
 }
