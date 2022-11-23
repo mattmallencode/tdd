222a3320ff6d8b448d38e4e15ada9e3c969f8360
Matt Mallen <120355103@umail.ucc.ie>
2022-11-23 20:24:52 +0000

    REFACTOR: removing the exception tests for each colour in testSettersUpperLimit() and testSettersLowerLimit(). Since all of these methods call the same validateRGBBeforeSet(), while this done in theory reduce code coverage, it's redundant to have these essentially duplicate assertions for each setter in the tests.

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -66,26 +66,19 @@ class ColourTest {
     }
 
     @Test
-    void testColourComparison() {
+    void testColourComparison3Params() {
         Colour firstColour = new Colour(1.0F, 1.0F, 1.0F);
         Colour secondColour = new Colour(1.0F, 1.0F, 1.0F);
         Assertions.assertEquals(firstColour, secondColour);
         firstColour.setRed(0.8F);
+        Assertions.assertNotEquals(firstColour, secondColour);
     }
 
     @Test
     void testSettersUpperLimit() {
-        Exception floatTooLargeExceptionRed = Assertions.assertThrows(IllegalArgumentException.class, () -> {
+        Exception floatTooLargeException = Assertions.assertThrows(IllegalArgumentException.class, () -> {
             Colour colour = new Colour(1.0F, 1.0F, 1.0F);
             colour.setRed(1.1F);
-        });
-        Exception floatTooLargeExceptionGreen = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Colour colour = new Colour(1.0F, 1.0F, 1.0F);
-            colour.setGreen(1.1F);
-        });
-        Exception floatTooLargeExceptionBlue = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Colour colour = new Colour(1.0F, 1.0F, 1.0F);
-            colour.setBlue(1.1F);
         });
     }
 
@@ -94,14 +87,6 @@ class ColourTest {
         Exception floatTooSmallExceptionRed = Assertions.assertThrows(IllegalArgumentException.class, () -> {
             Colour colour = new Colour(1.0F, 1.0F, 1.0F);
             colour.setRed(-0.1F);
-        });
-        Exception floatTooSmallExceptionGreen = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Colour colour = new Colour(1.0F, 1.0F, 1.0F);
-            colour.setGreen(-0.1F);
-        });
-        Exception floatTooSmallExceptionBlue = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Colour colour = new Colour(1.0F, 1.0F, 1.0F);
-            colour.setBlue(-0.1F);
         });
     }
 }
