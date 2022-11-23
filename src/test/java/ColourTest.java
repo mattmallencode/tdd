df7a66130cf1ca918b7105edc9fd59ed9da7d95f
Matt Mallen <120355103@umail.ucc.ie>
2022-11-23 14:21:47 +0000

    REFACTOR: Getting rid of testColourConstructor() as it simply tests creating an instance of colour - a test that is repeated by testParamUpperRangeException() and testParamLowerRangeException().

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -1,13 +1,7 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class ColourTest {
-    @Test
-    void testColourConstructor() {
-        Colour colour = new Colour(1.0F, 1.0F, 1.0F);
-    }
 
     @Test
     void testParamUpperRangeException() {
