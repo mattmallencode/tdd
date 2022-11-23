458baafc71df96502e13e00aebfb90893d79cf78
Matt Mallen <120355103@umail.ucc.ie>
2022-11-23 14:25:12 +0000

    REFACTOR: Making test variable names self documenting e.g. instead of "colour", "colourWithRedGreaterThanOne".

--- src/test/java/ColourTest.java
+++ src/test/java/ColourTest.java
@@ -2,11 +2,10 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class ColourTest {
-
     @Test
     void testParamUpperRangeException() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            Colour colour = new Colour(1.1F, 1.0F, 1.0F);
+            Colour colourWithRedGreaterThanOne = new Colour(1.1F, 1.0F, 1.0F);
         });
         Assertions.assertEquals("RGB params must all be below 1.0F", exception.getMessage());
     }
@@ -14,7 +13,7 @@ class ColourTest {
     @Test
     void testParamLowerRangeException() {
         Exception exception = Assertions.assertThrows(IllegalArgumentException.class, () -> {
-           Colour colour = new Colour(-0.1F, 0.0F, 0.0F);
+           Colour colourWithRedLessThanZero = new Colour(-0.1F, 0.0F, 0.0F);
         });
         Assertions.assertEquals("RGB params must all be equal to or greater than 0.F", exception.getMessage());
     }
