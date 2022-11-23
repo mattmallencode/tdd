65ebcaf8a83b29b1fbba38e8849b18d796e91a5a
Matt Mallen <120355103@umail.ucc.ie>
2022-11-23 14:13:13 +0000

    TEST PASS: testParamLowerRangeException() now passes as Colour raises an IllegalArgumentException a user creates a Colour instance with RGB params < 0F.

--- src/main/java/Colour.java
+++ src/main/java/Colour.java
@@ -9,7 +9,10 @@ public class Colour {
         this.green = green;
         this.blue = blue;
         if (red > 1.0F || green > 1.0F || blue > 1.0F) {
-            throw new IllegalArgumentException("RGB params must all be below 1.0F");
+            throw new IllegalArgumentException("RGB params must all be equal to or less than 1.0F");
+        }
+        if (red < 0.F || green < 0.F || blue < 0.F) {
+            throw new IllegalArgumentException();
         }
     }
 }
